c5869eecbbc1ae08ca8207cf59c28dedcfaba9ce
Zahra Hajihashemi <zahra.hajihashemi@gmail.com>
2013-07-27 22:17:16 +0000

    Update rewrite web.js

--- node-js-sample/web.js
+++ node-js-sample/web.js
@@ -1,16 +1,13 @@
+#!/usr/bin/env node
 var express = require('express');
-var app = express();
-app.use(express.logger());
-
+var app = express.createServer(express.logger());
+var fs=require('fs');
 app.get('/', function(request, response) {
-fs = require('fs');
-fs.readFile('/bitstarter/node-js-sample/index.html', 'utf8', function(err,data){
-  if (err) { return console.log(err);}
-  response.send(data);
- });
-});
+    //  response.send('Hello World2 !');
+    response.send(fs.readFileSync('index.html').toString()) ;
+  });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+   var port = process.env.PORT || 5000;
+   app.listen(port, function() {
+     console.log("Listening on " + port);
+   });
